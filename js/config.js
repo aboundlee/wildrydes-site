2e15c69e8ff30bb1b0123a9c50fc0b01d0510ce3
aboundlee <aboundlee@gmail.com>
2020-10-13 06:15:55 +0000

    config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_BmcKvvAd9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ob6v9qeg1agqc8agmtp7ci4m5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
