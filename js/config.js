d8ca2fafd9f5e874e93c868931d1eca06eaf448e
aboundlee <aboundlee@gmail.com>
2020-10-13 06:17:26 +0000

    d

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_BmcKvvAd9', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'ob6v9qeg1agqc8agmtp7ci4m5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
